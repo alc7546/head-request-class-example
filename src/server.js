e91fd8c924967caa3012dc37d92065dae910a926
Andrew Clark (RIT Student) <alc7546@ad.rit.edu>
2019-02-01 14:43:14 -0500

    2/1/2019 Exercise Head Requests

--- src/server.js
+++ src/server.js
@@ -8,7 +8,34 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  console.dir(request.method);
+  switch (request.method) {
+    case 'GET':
+      if (parsedUrl.pathname === '/') {
+        htmlHandler.getIndex(request, response);
+      } else if (parsedUrl.pathname === '/style.css') {
+        htmlHandler.getCSS(request, response);
+      } else if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsers(request, response);
+      } else if (parsedUrl.pathname === '/updateUser') {
+        jsonHandler.updateUser(request, response);
+      } else {
+        jsonHandler.notFound(request, response);
+      }
+      break;
 
+    case 'HEAD':
+      if (parsedUrl.pathname === '/getUsers') {
+        jsonHandler.getUsersMeta(request, response);
+      } else {
+        jsonHandler.notFound(request, response);
+      }
+      break;
+
+    default:
+      jsonHandler.notFound(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port);
